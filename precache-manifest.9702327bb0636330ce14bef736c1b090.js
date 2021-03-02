22fb96513e98cdbc13e7b109d474d7178747f7e4
olia <oliakuku25@gmail.com>
2021-03-02 18:26:29 +0200

    Updates

---
+++ precache-manifest.9702327bb0636330ce14bef736c1b090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3c0dc1232dc362812d4ed94248893d",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "43c877895a3642993b1b",
+    "url": "/movies/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "2e0c7030b5714edbfa79",
+    "url": "/movies/static/css/main.ef9f6d8e.chunk.css"
+  },
+  {
+    "revision": "43c877895a3642993b1b",
+    "url": "/movies/static/js/2.9930e1a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies/static/js/2.9930e1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0c7030b5714edbfa79",
+    "url": "/movies/static/js/main.0d359c49.chunk.js"
+  },
+  {
+    "revision": "45f1b12226fb8957f395",
+    "url": "/movies/static/js/runtime-main.6ee545b8.js"
+  }
+]);
